a91f9d6ce11e78ba15295b0d085016f643b0d0aa
Kat Tochowicz <ktochow1@binghamton.edu>
2020-05-30 14:00:11 -0400

    like model

---
+++ app/controllers/api/v1/likes_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::LikesController < ApplicationController
+
+
+end 
---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,46 @@
+class Api::V1::PostsController < ApplicationController
+
+  def index
+    render json: PostSerializer.new(Post.all)
+    #need instance variables when rendering views - not api data
+  end
+
+  def create
+     post = Post.new(post_params)
+     # byebug
+     if post.save
+       render json: PostSerializer.new(post), status: :accepted #send status code to fetch request tells client if request rejected or passed
+     else
+       render json: {errors: post.errors.fullmessages}, status: :unprocessible_entity #this can be displayed to frontend
+     end
+  end
+
+
+  # def show
+  #   post = Post.find(params[:id])
+  #   render json: PostSerializer.new(post)
+  # end
+  #
+  def update
+    post = Post.find_by(id: params[:id])
+    binding.pry
+  end
+
+  def edit
+    post = Post.find_by(id: params[:id])
+    if post.valid?
+      render json: post
+    else
+      render json: {errors: post.errors.fullmessages}, status: :unprocessible_entity
+    end
+  end
+
+  # def delete
+  # end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :image_url)
+  end
+end
