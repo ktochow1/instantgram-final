27d4cfe914f38392dd3733f9dff25a48ea9c709a
Kat Tochowicz <ktochow1@binghamton.edu>
2020-06-07 22:22:25 -0400

    EURIKA! EDIT BUTTON WORKS AS SHOULD

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,16 +1,16 @@
 class Api::V1::LikesController < ApplicationController
 
-
-
-end
-
   def index
     render json: LikeSerializer.new(Like.all)
   end
 
-  def show
-    like = Like.find(params[:id])
-    render json: LikeSerializer.new(like)
+  def create
+    like = Like.new(like_params)
+    if like.save
+      render json: like, status: :accepted
+    else
+      render json: {errors: like.errors.fullmessages}, status: :unprocessible_entity #this can be displayed to frontend
+    end
   end
 
   private
