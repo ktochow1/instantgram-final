16c4935bdc0cb62b6d349ec68dfe72894bf9d89d
Kat Tochowicz <ktochow1@binghamton.edu>
2020-06-07 20:10:04 -0400

    a

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,4 +1,12 @@
 class Api::V1::LikesController < ApplicationController
 
+  def index
+    render json: LikeSerializer.new(Like.all)
+  end
 
-end 
+  private
+
+  def like_params
+    params.require(:like).permit(:likes)
+  end
+end
---
+++ app/serializers/post_serializer.rb
@@ -0,0 +1,19 @@
+class PostSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :title, :image_url
+  has_many :likes
+  has_many :comments
+  # 
+  # def likes
+  #   self.object.likes.map do |like|
+  #     {like: like.likes}
+  #   end
+  # end
+  #
+  # def comments
+  #   self.object.comments.map do |comment|
+  #     {content: comment.content}
+  #   end
+  # end
+
+end
