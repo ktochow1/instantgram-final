a288f11223ade5aa9483e1e27627143cd37b77c3
Kat Tochowicz <ktochow1@binghamton.edu>
2020-06-07 20:15:49 -0400

    .

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_123033) do
+ActiveRecord::Schema.define(version: 2020_04_26_220804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2020_04_26_123033) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "post_id"
   end
+  # 
+  # create_table "likes", force: :cascade do |t|
+  #   t.integer "likes"
+  #   t.datetime "created_at", precision: 6, null: false
+  #   t.datetime "updated_at", precision: 6, null: false
+  #   t.integer "post_id"
+  # end
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
